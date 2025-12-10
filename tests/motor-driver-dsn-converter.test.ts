3e5711993c8fc30acabf749d4ff3cf175adea8e8
imrishabh18 <belikerishabh@gmail.com>
2025-12-10 20:53:36 +0530

    add the test

--- tests/motor-driver-dsn-converter.test.ts
+++ tests/motor-driver-dsn-converter.test.ts
@@ -5,18 +5,17 @@ import { convertDsnToCircuitJson } from "../lib/dsn-to-pcb/DsnToCircuitJsonConve
 import { convertCircuitJsonToPcbSvg } from "circuit-to-svg"
 
 test("can convert motor driver dsn to circuit json", async () => {
-    const dsnPath = resolve("site/repro01/assets/motor_driver_input.dsn")
-    const content = await readFile(dsnPath, "utf-8")
-  
-    const circuitJson = convertDsnToCircuitJson(content)
-  
-    expect(circuitJson).toBeDefined()
-    Bun.write(
-      "./debug-output/motor-driver-dsn-converter.json",
-      JSON.stringify(circuitJson, null, 2),
-    )
-  
-    const svg = convertCircuitJsonToPcbSvg(circuitJson)
-    expect(svg).toMatchSvgSnapshot(import.meta.path)
-  })
-  
+  const dsnPath = resolve("site/repro01/assets/motor_driver_input.dsn")
+  const content = await readFile(dsnPath, "utf-8")
+
+  const circuitJson = convertDsnToCircuitJson(content)
+
+  expect(circuitJson).toBeDefined()
+  Bun.write(
+    "./debug-output/motor-driver-dsn-converter.json",
+    JSON.stringify(circuitJson, null, 2),
+  )
+
+  const svg = convertCircuitJsonToPcbSvg(circuitJson)
+  expect(svg).toMatchSvgSnapshot(import.meta.path)
+})
